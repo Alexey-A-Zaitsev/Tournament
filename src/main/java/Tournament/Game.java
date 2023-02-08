9564a09d5a29e377fb2977caf495909bd1ccaee0
Alex Zaitsev <saizewalex@gmail.com>
2023-02-08 22:24:55 +0300

    Added HashMap; Tests edited

--- src/main/java/Tournament/Game.java
+++ src/main/java/Tournament/Game.java
@@ -1,31 +1,38 @@
 package Tournament;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 public class Game {
-    List<Player> players = new ArrayList<Player>();
+    //List<Player> players = new ArrayList<Player>();
+    private HashMap<String, Player> players = new HashMap();
 
+    public HashMap<String, Player> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(HashMap<String, Player> players) {
+        this.players = players;
+    }
 
     public void register (Player player) {
-        players.add(player);
+        players.put(player.getName(), player);
     }
 
     public int round (String playerName1, String playerName2) {
         Player player1 = null;
         Player player2 = null;
-        for (Player player : players) {
-            if (player.getName().equals(playerName1)) {
-                player1 = player;
-            }
-            if (player.getName().equals(playerName2)) {
-                player2 = player;
-            }
-        }
-        if (player1 == null) {
+
+        if (players.containsKey(playerName1)) {
+            player1 = players.get(playerName1);
+        } else {
             throw new NotRegisteredException(playerName1);
         }
-        if (player2 == null) {
+
+        if (players.containsKey(playerName2)) {
+            player2 = players.get(playerName2);
+        } else {
             throw new NotRegisteredException(playerName2);
         }
 
